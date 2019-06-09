7a844420022f24b4a4364dc002eb00c9e8aa726a
katylorny <48956039+katylorny@users.noreply.github.com>
2019-06-09 17:26:26 +0300

    вползание формы

--- js/form.js
+++ js/form.js
@@ -4,4 +4,7 @@ var button = document.querySelector(".hotel-search__button");
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
   form.classList.toggle("modal-close");
+  // form.classList.toggle("modal-open");
+
   })
+
