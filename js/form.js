b790de3c0d5d78a5607bb29e89f777ca12a085d0
katylorny <48956039+katylorny@users.noreply.github.com>
2019-06-09 11:33:07 +0300

    закончила верстку
    и добавила закрытие-открытие формы по клику. анимацию еще не делала

---
+++ js/form.js
@@ -0,0 +1,7 @@
+var form = document.querySelector(".search-form");
+var button = document.querySelector(".hotel-search__button");
+
+button.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  form.classList.toggle("modal-close");
+  })
