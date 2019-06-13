0309c276d98f1bb566268cea43b9ae0d4029771c
katylorny <48956039+katylorny@users.noreply.github.com>
2019-06-13 16:23:53 +0300

    изменения после предзащиты
    сделала кроме списка и доступности галочек + не переименовала js+ нет критериев д17 д18

--- js/form.js
+++ js/form.js
@@ -2,21 +2,13 @@ var form = document.querySelector(".search-form");
 var button = document.querySelector(".hotel-search__button");
 
 button.addEventListener("click", function (evt) {
-  // evt.preventDefault();
-  // form.classList.toggle("modal-close");
-  // form.classList.toggle("modal-open");
-
-  //form.classList.toggle("modal-close");
   if (form.classList.contains("modal-close")) {
-    //console.log("нет close");
-
     form.classList.remove("modal-close")
     form.classList.add("modal-open")
   }
-  else{
+  else {
     form.classList.remove("modal-open")
     form.classList.add("modal-close")
   }
-
-  })
+})
 
